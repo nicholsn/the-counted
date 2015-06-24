f6a31006cda62ffc08585a52725b965da8f41834
Nolan Nichols <nolan.nichols@gmail.com>
2015-06-23 21:15:45 -0700

    add: data download

---
+++ get_data.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""
+A script to downnload `the-counted.csv` dataset from http://interactive.guim.co.uk/2015/the-counted/thecounted-data.zip
+"""
+
+import os
+import datetime
+import zipfile
+
+import requests
+
+link = 'http://interactive.guim.co.uk/2015/the-counted/thecounted-data.zip'
+counted_zip = 'thecounted-data.zip'
+counted_csv = 'the-counted.csv'
+
+with open(counted_zip, 'wb') as fi:
+    print "Downloading {0}".format(link)
+    r = requests.get(link)
+    fi.write(r.content)
+
+with zipfile.ZipFile(counted_zip) as fi:
+    print "Unzipping {0}".format(counted_zip)
+    fi.extract(counted_csv)
+    os.remove(counted_zip)
+
+with open('README.md') as fi:
+    print "Updating README.md"
+    dt = datetime.datetime.now()
+    message = "\n- Updated csv on {0}".format(dt)
